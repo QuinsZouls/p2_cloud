09fcdb8ba44443085983e43a7242f4fa2cccefbc
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-04-04 11:50:46 -0500

    Add definitions

--- api/encrypt.ts
+++ api/encrypt.ts
@@ -1,12 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const algorithm = 'aes-256-cbc'; //Método AES encryption
+const secret = process.env.SECRET_KEY;
+//Para cifrar el texto
+async function encryptText(text: string) {
+  let txtEncrypted = '';
+  return txtEncrypted;
+}
+async function decryptText(encryptedText: string) {
+  let decryptedText = '';
+  return decryptedText;
+}
+
 export default function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+  const { message } = request.body;
+  const encryptedText = encryptText(message);
+
   response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
+    encryptedText,
   });
 }
